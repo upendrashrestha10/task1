2d21ceb307482a6caad9dca3895a025b1d23dc76
upendrashrestha10 <shresthaupendra1234@gmail.com>
2024-07-18 15:41:59 +0545

    my first task

---
+++ task2.py
@@ -0,0 +1,10 @@
+# Python program to check if a number is even or odd
+
+# Take user input
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
